682406ceb8fd21dec1202d05cb2cdba0b7b9393c
TashCash <36663394+TashCash@users.noreply.github.com>
2023-12-28 17:14:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # main.py
-import sys
 
 def merge_arrays(arr1, arr2):
     result_array = [x for x in arr1 if x in arr2]
     return result_array
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Использование: python main.py <arr1> <arr2>")
-        sys.exit(1)
-
-    arr1 = list(map(int, sys.argv[1].split(',')))
-    arr2 = list(map(int, sys.argv[2].split(',')))
+if name == "__main__":
+    arr1 = list(map(int, input("Введите элементы первого массива через пробел: ").split()))
+    arr2 = list(map(int, input("Введите элементы второго массива через пробел: ").split()))
 
     result = merge_arrays(arr1, arr2)
 
